955d26c6fb42a99e88b53c7b6ec9f0efe34f9f58
hariesmh <muhammadhariespnl@gmail.com>
2023-10-21 10:45:07 -0700

    Contoh Nama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,8 @@ package org.example;
 public class Main {gg
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+            System.out.printf("Muhammad Haries");
         }
     }
 }
